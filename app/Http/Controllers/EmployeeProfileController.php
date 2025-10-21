93260910be63200685e9af036694356ab4ba5418
Daiyan <daiyan1091@gmail.com>
2025-10-21 13:48:50 +0600

    employee service class

--- app/Http/Controllers/EmployeeProfileController.php
+++ app/Http/Controllers/EmployeeProfileController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\EmployeeServices;
 use Illuminate\Http\Request;
 
 class EmployeeProfileController extends Controller
 {
+    protected $empServices;
+    public function __construct(EmployeeServices $empServices){
+        $this->empServices = $empServices;
+    }
+
+    public function index(){
+        return view('employee_profile.index');
+    }
+
+    public function profileView($id){
+        $employee = $this->empServices->getEmployeeById($id);
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+
+        ]);
+    }
     public function showGeneralInfoFormShow()
     {
         $title = 'Employee General Information';
