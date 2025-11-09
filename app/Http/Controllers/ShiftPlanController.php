9b1952afb6bb316c30d4cd0bc785b075bcd4aeee
Rajib Kumar <rajib2516@student.nstu.edu.bd>
2025-11-09 15:01:50 +0600

    Add create method to ShiftPlanController for creating new shift plans

--- app/Http/Controllers/ShiftPlanController.php
+++ app/Http/Controllers/ShiftPlanController.php
@@ -23,6 +23,12 @@ class ShiftPlanController extends Controller
         $shiftPlans = $this->planServices->getPlans(ShiftPlan::class, 20);
         return view('plans.shift_plans.index', compact('title', 'section', 'sub_section', 'shiftPlans'));
     }
+    public function create(){
+        $title = 'Create Shift Plan';
+        $section = 'Plans Setup';
+        $sub_section = 'Shift Plan';
+        return view('plans.shift_plans.form', compact('title', 'section', 'sub_section'));
+    }
     public function store(Request $request){
         $validated = $this->planServices->shiftPlanValidation($request);
 
