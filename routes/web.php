d473801a1a6e539b9a950b3e288422430b2bbb1d
Rajib Kumar <rajib2516@student.nstu.edu.bd>
2025-11-30 14:43:56 +0600

    Add leave plans view with detailed list and selection functionality

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ use App\Http\Controllers\EmployeePlansController;
 
 
 Route::get('test', function () {
-   return view('employee.profile_view');
+   return view('leave');
 });
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
