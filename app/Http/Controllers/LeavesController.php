63981e37a94c85f665fc647b6b644df0916a0ebb
Daiyan <daiyan1091@gmail.com>
2025-12-02 16:52:57 +0600

    Leaves Module Model and migration

--- app/Http/Controllers/LeavesController.php
+++ app/Http/Controllers/LeavesController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
+use App\Models\Leave;
+use App\Models\LeavePlan;
 use Illuminate\Http\Request;
 
 class LeavesController extends Controller
 {
     public function index(){
+        $leaves = Leave::all();
 
     }
     public function create(){
-
+        $employees = Employee::all();
+        $plans = LeavePlan::all();
     }
 }
