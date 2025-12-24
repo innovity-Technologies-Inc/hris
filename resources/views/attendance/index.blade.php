cde8d46ae5020354096fe950a6cb09216b09d0a0
Daiyan <daiyan1091@gmail.com>
2025-12-24 13:18:18 +0600

    Attandance Module Backend Complete

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -170,6 +170,7 @@
                                 <tr class="text-uppercase small fw-semibold text-muted">
                                     <th scope="col" class="py-3">Employee ID</th>
                                     <th scope="col" class="py-3">Employee Name</th>
+                                    <th scope="col" class="py-3" >Shift Type</th>
                                     <th scope="col" class="py-3" style="width: 160px;">Shift</th>
                                     <th scope="col" class="py-3 d-none d-md-table-cell">Clock In</th>
                                     <th scope="col" class="py-3 d-none d-md-table-cell text-center">In Status</th>
@@ -196,24 +197,37 @@
                                                 style="background-color: var(--bs-secondary-bg); color: var(--bs-body-color);">{{ $record->getEmployee->full_name }}</code>
                                         </td>
 
+                                        <td class="py-3">
+                                            <code class="px-2 py-1 rounded small"
+                                                  style="background-color: var(--bs-secondary-bg); color: var(--bs-body-color);">{{ $record->shift_type }}</code>
+                                        </td>
+
                                         {{-- Shift (Fixed Width, Two-Line Display) --}}
-                                        {{--<td class="py-3" style="width: 160px;">
+                                        <td class="py-3" style="width: 160px;">
                                             <div class="d-flex align-items-start">
                                                 <svg class="me-2 mt-1 flex-shrink-0 text-primary" width="14"
                                                     height="14" fill="currentColor" viewBox="0 0 20 20">
                                                     <path fill-rule="evenodd"
                                                         d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" />
                                                 </svg>
-                                                <div style="line-height: 1.3;">
+                                                {{--<div style="line-height: 1.3;">
                                                     <div class="fw-medium" style="font-size: 0.813rem;">
-                                                        {{ $record->getShift->name }}</div>
+                                                        @if($shift_type == 'Regular')
+                                                            {{ $record->getEmployee->shift->getPlan->name }}
+                                                        @elseif($shift_type == 'Roster')
+                                                            {{ $record->getEmployee->roster->getPlan->name }}
+                                                        @endif
+                                                    </div>
                                                         <div class="text-muted"
                                                             style="font-size: 0.688rem; white-space: nowrap;">
+                                                            @if($shift_type == 'Regular')
                                                             {{ \Carbon\Carbon::parse($record->getShift->clock_in_time)->format('h:i A') }} -
-                                                            {{ \Carbon\Carbon::parse($record->getShift->clock_out_time)->format('h:i A') }}</div>
-                                                </div>
+                                                            {{ \Carbon\Carbon::parse($record->getShift->clock_out_time)->format('h:i A') }}
+                                                            @endif
+                                                        </div>
+                                                </div>--}}
                                             </div>
-                                        </td>--}}
+                                        </td>
 
                                         {{-- Clock In --}}
                                         <td class="py-3 d-none d-md-table-cell">
