a7c39b48130be61afd28ddd8dee06bf33d1ef961
Daiyan <daiyan1091@gmail.com>
2026-05-10 15:22:25 +0600

    style: align salary view action button with project standards

--- resources/views/payroll/salary/view.blade.php
+++ resources/views/payroll/salary/view.blade.php
@@ -62,8 +62,8 @@
                                             ৳ {{ number_format($item->total_salary, 2) }}
                                         </td>
                                         <td class="text-center">
-                                            <a href="{{ route('salary.payroll.show', $item->id) }}" class="btn btn-outline-primary btn-sm rounded-circle" title="View Details">
-                                                <i class="fas fa-eye"></i>
+                                            <a href="{{ route('salary.payroll.show', $item->id) }}" class="btn btn-info btn-sm" title="View Details">
+                                                <i style="height: 12px; width: 12px" data-feather="eye"></i>
                                             </a>
                                         </td>
                                     </tr>
