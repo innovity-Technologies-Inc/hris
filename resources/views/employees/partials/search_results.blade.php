7eb344c387e0c642aa5bed3e67cb4cef1b0f6073
Daiyan <daiyan1091@gmail.com>
2026-05-18 18:20:49 +0600

    fix: resolve layout alignment issues on employee list page

--- resources/views/employees/partials/search_results.blade.php
+++ resources/views/employees/partials/search_results.blade.php
@@ -1,61 +1,61 @@
-<div class="card-body p-0">
+<div class="p-0">
     @can('employee-management.create')
-    <button type="button" class="btn btn-warning btn-sm me-3 mb-3" data-bs-toggle="modal" data-bs-target="#createOptionsModal">
-        <i style="height: 12px; width: 12px" data-feather="plus"></i> Create
-    </button>
+        <div class="d-flex justify-content-end mb-3">
+            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#createOptionsModal">
+                <i style="height: 12px; width: 12px" data-feather="plus"></i> Create
+            </button>
+        </div>
     @endcan
 
     @if ($employees->isEmpty())
-        <div class="text-center py-4 text-muted">No employees found.</div>
+        <div class="text-center py-4 text-muted border rounded">No employees found.</div>
     @else
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered mb-0">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Profile</th>
-                            <th scope="col">System ID</th>
-                            <th scope="col">Employee ID</th>
-                            <th scope="col">Employee Name</th>
-                            @can('employee-management.view')
+        <div class="table-responsive">
+            <table class="table table-bordered mb-0">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Profile</th>
+                        <th scope="col">System ID</th>
+                        <th scope="col">Employee ID</th>
+                        <th scope="col">Employee Name</th>
+                        @can('employee-management.view')
                             <th scope="col" style="width: 120px;">Action</th>
-                            @endcan
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                            $sl = \App\HelperClass::indexNumberSerialization($employees);
-                        @endphp
-                        @foreach ($employees as $employee)
-                            <tr>
-                                <th scope="row">{{ $sl++ }}</th>
-                                <td>
-                                    {!! \App\HelperClass::generateAvatar(
-                                        $employee->photo_path,
-                                        $employee->full_name,
-                                        32,
-                                        '#974063',
-                                        '',
-                                        $employee->id,
-                                    ) !!}
-                                </td>
-                                <td>{{ $employee->system_id }}</td>
-                                <td>{{ $employee->applicant_id }}</td>
-                                <td>{{ $employee->full_name }}</td>
-                                @can('employee-management.view')
+                        @endcan
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                        $sl = \App\HelperClass::indexNumberSerialization($employees);
+                    @endphp
+                    @foreach ($employees as $employee)
+                        <tr>
+                            <th scope="row">{{ $sl++ }}</th>
+                            <td>
+                                {!! \App\HelperClass::generateAvatar(
+                                    $employee->photo_path,
+                                    $employee->full_name,
+                                    32,
+                                    '#974063',
+                                    '',
+                                    $employee->id,
+                                ) !!}
+                            </td>
+                            <td>{{ $employee->system_id }}</td>
+                            <td>{{ $employee->applicant_id }}</td>
+                            <td>{{ $employee->full_name }}</td>
+                            @can('employee-management.view')
                                 <td>
                                     <a href="{{ route('employees.profile.general_informations', $employee->id) }}"
                                         class="btn btn-secondary btn-sm" title="View">
                                         <i style="height: 12px; width: 12px" data-feather="eye"></i>
                                     </a>
                                 </td>
-                                @endcan
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                            @endcan
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
 
         <div class="mt-3">
@@ -71,5 +71,4 @@
             }
         });
     </script>
-
 </div>
